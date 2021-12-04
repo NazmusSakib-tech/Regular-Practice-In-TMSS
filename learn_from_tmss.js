f8e3f3bd692495173ba3be2b3a8b57c339d26a99
NazmusSakib-tech <sakibkhancse09@gmail.com>
2021-12-04 09:42:52 +0600

    first commit

---
+++ learn_from_tmss.js
@@ -0,0 +1,8 @@
+// INV-20-Seeder-For-Suppplire-Brand
+
+// php artisan db:seed
+// php artisan migrate:refresh --seed
+// php artisan cache:forget spatie.permission.cache
+// php artisan cache:clear 
+
+// php artisan make:seeder ProductCategorySeeder
